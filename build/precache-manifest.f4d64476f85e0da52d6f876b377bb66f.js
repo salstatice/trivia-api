30034619a1999e7617392a879c0dd418f7fb5ceb
Unkei Kou <72512389+salstatice@users.noreply.github.com>
2021-06-03 17:55:48 -0400

    Chore: npm build

---
+++ build/precache-manifest.f4d64476f85e0da52d6f876b377bb66f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d708a60c913f8e853fd39b71f5b64e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f3b759f2c4e86fe0314",
+    "url": "/static/css/main.e8146a77.chunk.css"
+  },
+  {
+    "revision": "549f42bc82c41b5e7bb8",
+    "url": "/static/js/2.1a92cffe.chunk.js"
+  },
+  {
+    "revision": "5f3b759f2c4e86fe0314",
+    "url": "/static/js/main.3c2b3ed6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
