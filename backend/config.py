bb5d753165c665ae85c96c172064acea088ad379
Unkei Kou <72512389+salstatice@users.noreply.github.com>
2021-06-03 22:50:36 -0400

    Chore: change database url

--- backend/config.py
+++ backend/config.py
@@ -5,6 +5,6 @@ DB_USER = os.getenv('DB_USER', 'postgres')
 DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
 DB_NAME = os.getenv('DB_NAME', 'your_db')
 
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql+psycopg2://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
