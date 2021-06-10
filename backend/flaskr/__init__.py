c92f915268ce0e106693c8072cd1db233f3ccd83
Unkei Kou <72512389+salstatice@users.noreply.github.com>
2021-06-10 15:52:55 -0400

    Attempt to deploy static file #2

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, abort, jsonify, send_static_file
+from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
